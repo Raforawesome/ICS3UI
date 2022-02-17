89e3b850ae9b3b39cc6169a2d9b1f47cbcecea17
Tahir Chaudhry <tahirchaudhry@Tahirs-Air.phub.net.cable.rogers.com>
2022-02-17 18:27:07 -0500

    cleaned up directories with .gitignore

---
+++ README.md
@@ -0,0 +1,2 @@
+## ICS3UI Projects
+A collection of all my projects I coded for the ICS3UI-02 class.  Ignore everything under /lib, those are just libraries I'm working on to make Python slightly less frustrating.
--- input-checker.py
+++ input-checker.py
@@ -17,83 +17,83 @@ def run():  # make a function and use recursion to repeat the program
 		"Enter an integer less than 50: ",
 		"Enter a decimal number: ",
 		"Enter a string that begins with a vowel: ",
-		"Enter a string with a consonant: "
+		"Enter a string that ends with a consonant: "
 	]
 
 
 	for i in range(1, len(questions) + 1):  # add one because range loops are exclusive
 		question = questions[i - 1]  # subtract one to account for zero-indexing
-		match i:  # only works in python 3.10+
-			case 1:  # 4 digit whole number
-				_input = input(question)  # prefix variable with underscore as 'input' is already a global
-				check = True  # start off being true
-				message = ""  # make our message change depending on what exactly is wrong
 
-				if not len(_input) == 4:  # our input must be 4 digits
-					check = False
-					message = "Input must be 4 characters!\n"
+		if i == 1:  # 4 digit whole number
+			_input = input(question)  # prefix variable with underscore as 'input' is already a global
+			check = True  # start off being true
+			message = ""  # make our message change depending on what exactly is wrong
 
-				if not _input.isnumeric() and check:  # check if our input consists of only numbers
-					check = False
-					message = "Whole numbers must be integers!\n"
-				
-				if check and not int(_input) >= 0:  # check if our number is above 0 (no negatives)
-					check = False
-					message = "Whole numbers must be positive!\n"
+			if not len(_input) == 4:  # our input must be 4 digits
+				check = False
+				message = "Input must be 4 characters!\n"
 
-				if check:  # if none of those if statements set check to false, send success message
-					message = "That works!\n"
-				print(message)
+			if not _input.isnumeric() and check:  # check if our input consists of only numbers
+				check = False
+				message = "Whole numbers must be integers!\n"
+			
+			if check and not int(_input) >= 0:  # check if our number is above 0 (no negatives)
+				check = False
+				message = "Whole numbers must be positive!\n"
+
+			if check:  # if none of those if statements set check to false, send success message
+				message = "That works!\n"
+			print(message)
 
 
-			case 2:  # integer below 50
-				_input = input(question)  # prefix variable with underscore as 'input' is already a global
+		elif i == 2:  # integer below 50
+			_input = input(question)  # prefix variable with underscore as 'input' is already a global
 
-				try:  # see if casting input to int errors
-					int(_input)
-				except:
-					print("This number isn't an integer.\n")
+			try:  # see if casting input to int errors
+				int(_input)
+			except:
+				print("This number isn't an integer.\n")
+			else:
+				num = int(_input)
+
+				if num >= 50:
+					print("This integer isn't below 50!\n")
 				else:
-					num = int(_input)
-
-					if num >= 50:
-						print("This integer isn't below 50!\n")
-					else:
-						print("That works!\n")
+					print("That works!\n")
 
 
-			case 3:
-				_input = input(question)  # prefix variable with underscore as 'input' is already a global
-				
-				try:
-					float(_input)
-				except:
-					print('Provided input is not a valid float.\n')
-				else:
-					if not float(_input) == math.floor(float(_input)):
-						print("That works!\n")
-					else:
-						print('This number is not a decimal.\n')
-
-
-			case 4:
-				_input = input(question)  # prefix variable with underscore as 'input' is already a global
-				char = _input[:1]
-
-				if char.lower() in vowels:
+		elif i == 3:
+			_input = input(question)  # prefix variable with underscore as 'input' is already a global
+			
+			try:
+				float(_input)
+			except:
+				print('Provided input is not a valid decimal number.\n')
+			else:
+				if not _input.isnumeric():
 					print("That works!\n")
 				else:
-					print("The first character of this string is not a vowel!\n")
+					print("That isn't a decimal number!\n")
 
 
-			case 5:
-				_input = input(question)  # prefix variable with underscore as 'input' is already a global
-				char = _input[:1]
+		elif i == 4:
+			_input = input(question)  # prefix variable with underscore as 'input' is already a global
+			char = _input[:1]
 
-				if not char.lower() in vowels:
-					print("That works!\n")
-				else:
-					print("The first character of this string is not a consonant!\n")
+			if char.lower() in vowels:
+				print("That works!\n")
+			else:
+				print("The first character of this string is not a vowel!\n")
+
+
+		elif i == 5:
+			_input = input(question)  # prefix variable with underscore as 'input' is already a global
+			char = _input[len(_input) - 1:len(_input)]
+
+			if (char.lower() in vowels) == False:
+				print("That works!\n")
+			else:
+				print("The last character of this string is not a consonant!\n")
 
 
 	rerun = True  # default value doesn't matter
