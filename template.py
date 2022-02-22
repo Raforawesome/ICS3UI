5d3b956378231ef762f05720555130b260a90d62
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-02-22 12:53:48 -0500

    reorganized files into folders

--- template.py
+++
@@ -1,8 +0,0 @@
-# Class template - to be used for all questions
-"""
-Practice Problem, week ??? OR Assignment question, No/week
-Author: Raforawesome
-Due Date: None
-Question or problem:
-Class: ICS3UI-02
-"""
